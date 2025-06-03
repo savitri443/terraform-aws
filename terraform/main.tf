90c3326509e5e23aa06f6ba8f02ed40d34d18f8e
savitri443 <savitri443@gmail.com>
2025-06-03 17:22:52 +0100

    removed provider details

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
-provider "aws" {
-  region = "us-east-1"
-}
 
 resource "aws_s3_bucket" "savitri_bucket" {
   bucket = "savitri-unique-bucket-1-terraform" # a unique name
